54fbc3de57d35825d67150f1b53a1115c5ad485c
inzahan <inzahanyang@gmail.com>
2020-04-10 23:14:09 +0900

    7.2

--- src/Routes/Feed.js
+++ src/Routes/Feed.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Helmet from "react-helmet";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
 import styled from "styled-components";
 import Loader from "../Components/Loader";
+import Post from "../Components/Post";
 
 const FEED_QUERY = gql`
   {
@@ -39,11 +41,35 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  min-height: 80vh;
+  min-height: 70vh;
 `;
 
 export default () => {
   const { data, loading } = useQuery(FEED_QUERY);
-  console.log(data, loading);
-  return <Wrapper>{loading && <Loader />}</Wrapper>;
+  console.log(data);
+  return (
+    <Wrapper>
+      <Helmet>
+        <title>Feed | Prismagram</title>
+      </Helmet>
+      {loading && <Loader />}
+      {!loading &&
+        data &&
+        data.seeFeed &&
+        data.seeFeed.map((post) => (
+          <Post
+            key={post.id}
+            id={post.id}
+            location={post.location}
+            caption={post.caption}
+            user={post.user}
+            files={post.files}
+            likeCount={post.likeCount}
+            isLiked={post.isLiked}
+            comments={post.comments}
+            createdAt={post.createdAt}
+          />
+        ))}
+    </Wrapper>
+  );
 };
