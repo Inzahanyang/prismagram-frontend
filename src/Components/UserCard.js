908c4ede24a2b5e24b553d520f945f9a7e402a0d
inzahan <inzahanyang@gmail.com>
2020-04-28 09:27:21 +0900

    8.2

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -4,13 +4,31 @@ import styled from "styled-components";
 import Avatar from "./Avatar";
 import FatText from "./FatText";
 import Button from "./Button";
+import { Link } from "react-router-dom";
 
-const Card = styled.div``;
+const Card = styled.div`
+  ${(props) => props.theme.whiteBox};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
+
+const EAvartar = styled(Avatar)`
+  margin-bottom: 15px;
+`;
+
+const ELink = styled(Link)`
+  color: inherit;
+  margin-bottom: 10px;
+`;
 
 const UserCard = ({ url, username, isSelf, isFollowing }) => (
   <Card>
-    <Avatar url={url} />
-    <FatText text={username} />
+    <EAvartar url={url} size={"md"} />
+    <ELink to={`/${username}`}>
+      <FatText text={username} />
+    </ELink>
     {!isSelf && <Button text={isFollowing ? "Unfollow" : "Follow"} />}
   </Card>
 );
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -10,7 +10,14 @@ const Wrapper = styled.div`
   text-align: center;
 `;
 
-const Section = styled.div``;
+const Section = styled.div`
+  margin-bottom: 50px;
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 160px;
+  grid-auto-rows: 160px;
+`;
 
 const SearchPresenter = ({ searchTerm, loading, data }) => {
   if (searchTerm === undefined) {
